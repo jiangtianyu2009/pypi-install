908ac28d9b97fe71fb1dbb3c72dfd8d40823c3de
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2022-10-23 13:54:26 +0800

    Update pypi_install.py

--- pypi_install.py
+++ pypi_install.py
@@ -1,12 +1,15 @@
+import subprocess
 import sys
 from pip import main as pip_main
 
 
 def install(package):
-    pip_main(['download', '-d', 'tmp_pkgs', package])
+    subprocess.call(['python', '-m', 'pip', 'download',
+                    '-d', 'tmp_pkgs', package])
 
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as f:
         for line in f:
             install(line)
+            print("##################################################")
